2c8a035087f99ddfbae934c1f9b82a8b72ca61ca
mazcode <maxliljh@163.com>
2026-07-19 02:33:18 +0800

    新增事件概念复习，”标准“事件写法StandardEventPartternDemo这个文件

---
+++ concepts/language-features/Demo02_Event/Program.cs
@@ -0,0 +1,140 @@
+﻿namespace Demo02_Event
+{
+
+    public class NewPublisher
+    {
+        // 直接暴露委托字段
+        public Action<string>? OnNewPublished;
+
+        public void Publish(string news)
+        {
+            OnNewPublished?.Invoke(news);
+        }
+    }
+
+    #region 事件标准用法
+
+   
+    //1. 自定义事件参数
+    public class NewEventArgs : EventArgs
+    {
+        public string HeadLine { get; set; }
+
+        public DateTime PublishTime { get; set; }
+
+        public NewEventArgs(string headline)
+        {
+            HeadLine = headline;
+            PublishTime = DateTime.Now;
+        }
+    }
+    //2.使用事件给委托加一把"安全锁",
+    //  声明事件发布者
+    public class NewsPublisher
+    {
+        //使用event 关键字 声明事件
+        public event EventHandler<NewEventArgs>? NewsPublished;
+
+        public void Publish(string headlines)
+        {
+            Console.WriteLine($"发布新闻");
+            //触发事件
+            OnNewPublished(new NewEventArgs(headlines));
+        }
+
+        //protected 修饰，类内部调用保护触发方法
+        //这里封装了触发逻辑：未来可以添加日志之类的工具调用。
+        //使用virtual ：则支持继承，子类（派生类）可以重写override这个方法
+        //一般用OnXXX的方式命名
+        protected virtual void OnNewPublished(NewEventArgs e)
+        {
+            Console.WriteLine($"当前订阅数量:{NewsPublished?.GetInvocationList().Count()}");
+            //集中处理，避免到处空引用检查
+            NewsPublished?.Invoke(this, e);
+        }
+    }
+
+    //3. 事件订阅者，事件触发处理事件
+    public class NewsSubscriber
+    {
+        private string  _name;
+
+        private NewsPublisher _publisher;
+
+        public NewsSubscriber(string name,NewsPublisher publisher)
+        {
+            _name = name;
+            //订阅事件
+            _publisher=publisher;//这里大概要判断传进来的publisher是否为空好点。
+
+            _publisher.NewsPublished += HandleNews;
+        }
+
+        private void HandleNews(object? sender, NewEventArgs e)
+        {
+            Console.WriteLine($"[{_name}] 收到消息：{e.HeadLine}；时间：{e.PublishTime}");
+        }
+
+        public void Unsubscribe()
+        {
+            //仅卸载当前类实例的事件订阅
+            _publisher?.NewsPublished -= HandleNews;
+        }
+    }
+    #endregion
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 如果直接暴露委托
+            //BadBad();
+            #endregion
+
+            //标准事件
+            UsingCase.Run();
+        }
+
+        private static void BadBad()
+        {
+            var publisher = new NewPublisher();
+
+            //订阅者A注册回调
+            publisher.OnNewPublished += msg => Console.WriteLine($"A收到：{msg}");
+            //订阅者B注册回调
+            publisher.OnNewPublished += msg => Console.WriteLine($"B收到：{msg}");
+
+            publisher.Publish("Breaking News!!!!");
+
+            //===如果订阅突然被清空了
+            publisher.OnNewPublished = null;
+            //Guess what
+            publisher.Publish("Morning News!!!!!");
+        }
+
+        private static void GoodGood()
+        {
+            //事件通过+=、-= 这两个操作符来执行订阅和取消订阅
+            //创建发布者实例
+            var publisher = new NewsPublisher();
+
+            //创建订阅者，构造方法内部自动订阅事件
+            var subA = new NewsSubscriber("订阅者A",publisher);
+            var subB = new NewsSubscriber("订阅者B",publisher);
+
+            //发布新闻
+
+            publisher.Publish("财经新闻");
+            publisher.Publish("科技新闻");
+
+            //A 取消订阅
+
+            subA.Unsubscribe();
+            publisher.Publish("体育新闻");
+
+        }
+
+
+    }
+}
---
+++ concepts/language-features/Demo02_Event/StandardEventPartternDemo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace Demo02_Event
+{
+
+    // 事件“标准”写法
+    //1. 声明一个EventArgs
+    //没数据也建议声明定义一个，以后扩展用得着。
+    public class DownloadProgressEventArgs : EventArgs
+    {
+        public int Percentage { get; }
+        public long BytesReceived { get; }
+
+        public DownloadProgressEventArgs(int percentage, long bytes)
+        {
+            Percentage = percentage;
+            BytesReceived = bytes;
+        }
+    }
+
+
+    //2. 声明发布者
+    public class FileDownloader
+    {
+        //2.1 使用EventHandler<T> 标准参数签名(sender,e)
+        //2.2 使用event声明这是一个事件。
+        //2.3 通常用ed结尾，毕竟都是干完活了。
+        //2.4 事件有可能为空，记得处理。
+        public event EventHandler<DownloadProgressEventArgs>? ProgressChanged;
+
+        public event EventHandler? DownloadCompleted;
+
+        //模拟过程
+        public void Start()
+        {
+            Console.WriteLine($"开始下载，当前时间{DateTime.Now}");
+
+            for (int i = 0; i <= 100; i += 20)
+            {
+                //3. 触发事件 
+                OnProgressChanged(new DownloadProgressEventArgs(i, i * 1024));
+                //模拟耗时
+                Thread.Sleep(500);
+            }
+
+            //模拟完成
+            OnDownloadCompleted(EventArgs.Empty);
+        }
+
+        // 4. 定义触发方法
+        //protected virtual:允许子类重写，在触发前后插入逻辑
+        protected virtual void OnProgressChanged(DownloadProgressEventArgs e)
+        {
+            //this 表示，告诉订阅者这是我发出的消息
+            ProgressChanged?.Invoke(this, e);
+        }
+
+        protected virtual void OnDownloadCompleted(EventArgs e)
+        {
+            DownloadCompleted?.Invoke(this, e);
+        }
+    }
+
+    /// <summary>
+    /// 用例
+    /// </summary>
+    public class UsingCase
+    {
+        public static void Run()
+        {
+            var downloader = new FileDownloader();
+
+            //订阅进度
+            //sender
+            downloader.ProgressChanged += (sender, e) =>
+            {
+                Console.WriteLine($"[进度更新]：{new string('#', e.Percentage / 5),-20} {e.Percentage}%；{e.BytesReceived}");
+            };
+
+            downloader.DownloadCompleted += (sender, e) =>
+            {
+                Console.WriteLine($"[完成时间]：{DateTime.Now}");
+            };
+
+            //发布事件
+            downloader.Start();
+        }
+    }
+}
